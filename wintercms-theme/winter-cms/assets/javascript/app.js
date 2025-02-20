c43766c98ec6eae40797e1ae3df1a3038185947a
HANSGMBH\mknop <mknop@hans-natur.de>
2025-02-20 12:04:44 +0100

    changes for theme

---
+++ wintercms-theme/winter-cms/assets/javascript/app.js
@@ -0,0 +1,70 @@
+/*
+ * Application
+ */
+
+
+$(window).load(function() {
+    
+});
+
+function autocompleteFunction(inputField) {
+    inputField.focus();
+    inputField.autocomplete({
+        source: function(request, response) {
+            $.ajax('onAutocomplete', {
+                type: 'POST',
+                data: {
+                    term: request.term
+                },
+                success: function(data) {
+                    if(data.length) {
+                        response(data);
+                    }
+                }
+            })
+        },
+        minLength: 3
+    });
+}
+
+/*
+ * Auto hide navbar
+ */
+jQuery(document).ready(function($){
+    var $header = $('.navbar-autohide'),
+        scrolling = false,
+        previousTop = 0,
+        currentTop = 0,
+        scrollDelta = 10,
+        scrollOffset = 150
+
+    $(window).on('scroll', function(){
+        if (!scrolling) {
+            scrolling = true
+
+            if (!window.requestAnimationFrame) {
+                setTimeout(autoHideHeader, 250)
+            }
+            else {
+                requestAnimationFrame(autoHideHeader)
+            }
+        }
+    })
+
+    function autoHideHeader() {
+        var currentTop = $(window).scrollTop()
+
+        // Scrolling up
+        if (previousTop - currentTop > scrollDelta) {
+            $header.removeClass('is-hidden')
+        }
+        else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+            // Scrolling down
+            $header.addClass('is-hidden')
+        }
+
+        previousTop = currentTop
+        scrolling = false
+    }
+
+});
